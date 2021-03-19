ded1c43aec83fbe536177b2b30d2a14d4c840eba
Danielson Alencar <danielson150@gmail.com>
2021-03-19 20:29:05 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Primeiro programa em Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World!',
+              style: TextStyle(fontSize: 40, color: Colors.red[900])),
+        ),
+      ),
+    );
+  }
+}
